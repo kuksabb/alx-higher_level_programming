499bcea0e69850a63cd069e922798fa6e7d430e6
kuksabb <111008120+kuksabb@users.noreply.github.com>
2022-12-21 21:15:47 +0000

    add

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         while count < x:
-            print("{}".format(my_list[count], end="")
+            print("{}".format(my_list[count]), end='')
             count += 1
         print()
         return count
